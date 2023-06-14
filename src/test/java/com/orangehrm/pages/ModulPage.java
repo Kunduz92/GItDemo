d8706b56da346b20d1ce11d7712f2548eb088140
Kunduz <kunduz.sydykova@gmail.com>
2023-06-14 11:00:16 -0400

    Method added

--- src/test/java/com/orangehrm/pages/ModulPage.java
+++ src/test/java/com/orangehrm/pages/ModulPage.java
@@ -2,6 +2,13 @@ package com.orangehrm.pages;
 
 public class ModulPage {
 
+    // create methods
 
-  # create methods
+    int a = 1;
+    int b  =2;
+
+    public int sum(){
+        return a+b;
+
+    }
 }
